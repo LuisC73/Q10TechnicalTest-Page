34122c8d1361d1db22a09996634d8b8c5a17fd58
Miguel Castro <luiscastroc11@gmail.com>
2025-01-25 12:39:38 -0500

    Feat: Added animated hamburger button for the menu

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 import 'boxicons/css/boxicons.min.css';
 import './style.css';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const buttonHamburger = document.querySelector('.hamburger-menu');
+
+  document.addEventListener('click', (e) => {
+    if (e.target === buttonHamburger) {
+      buttonHamburger.classList.toggle('hamburger-menu--active');
+    }
+  });
+});
