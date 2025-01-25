cf0c33c7fd66e0f6184df65695fb4396d3cea8fb
Miguel Castro <luiscastroc11@gmail.com>
2025-01-25 13:34:15 -0500

    Feat: Added menu functionality for mobile devices

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import './style.css';
 
 window.addEventListener('DOMContentLoaded', () => {
   const buttonHamburger = document.querySelector('.hamburger-menu');
+  const navbar = document.querySelector('.header__nav');
+
+  function toggleMenu() {
+    buttonHamburger.classList.toggle('hamburger-menu--active');
+    navbar.classList.toggle('header__nav--active');
+  }
 
   document.addEventListener('click', (e) => {
-    if (e.target === buttonHamburger) {
-      buttonHamburger.classList.toggle('hamburger-menu--active');
-    }
+    if (e.target === buttonHamburger) toggleMenu();
   });
 });
