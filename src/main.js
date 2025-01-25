bba0663e48ef4ec888342c30c9baf1f9871b9ac1
Miguel Castro <luiscastroc11@gmail.com>
2025-01-25 13:53:23 -0500

    Feat: Added a button to scroll back to the top

--- src/main.js
+++ src/main.js
@@ -1,16 +1,33 @@
 import 'boxicons/css/boxicons.min.css';
 import './style.css';
 
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   const buttonHamburger = document.querySelector('.hamburger-menu');
   const navbar = document.querySelector('.header__nav');
+  const buttonUp = document.querySelector('.button-up');
 
-  function toggleMenu() {
+  const toggleMenu = () => {
     buttonHamburger.classList.toggle('hamburger-menu--active');
     navbar.classList.toggle('header__nav--active');
-  }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
   document.addEventListener('click', (e) => {
     if (e.target === buttonHamburger) toggleMenu();
+    if (e.target === buttonUp) scrollToTop();
+  });
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 120) {
+      buttonUp.classList.add('button-up--active');
+    } else {
+      buttonUp.classList.remove('button-up--active');
+    }
   });
 });
